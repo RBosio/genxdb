aa1c7ef4f67cda7d7ac6884b5f2dad8beb067cec
Fiido <roccobosio@gmail.com>
2024-02-12 18:04:06 -0300

    set title

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log('hello world!')
+#!/usr/bin/env node
+
+import shelljs from "shelljs";
+import inquirer from "inquirer";
+import chalk from "chalk";
+import figlet from "figlet";
+
+const init = async () => {
+  console.clear();
+  console.log(
+    chalk.green(
+      figlet.textSync("GenYDB", {
+        // font: "Doh",
+        font: "Doom",
+        horizontalLayout: "default",
+        verticalLayout: "default",
+        width: 120,
+        whitespaceBreak: true,
+      })
+    )
+  );
+};
+
+init();
