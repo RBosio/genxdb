2b2e1ef05b73f94d4cfd4aa3134e8b5980bbac4e
Fiido <roccobosio@gmail.com>
2024-02-12 22:33:06 -0300

    create files

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,9 @@ import shelljs from "shelljs";
 import inquirer from "inquirer";
 import chalk from "chalk";
 import figlet from "figlet";
+import { join } from "path";
+
+import jsonFile from "jsonfile";
 
 const questions = [
   {
@@ -46,7 +49,33 @@ const init = async () => {
     )
   );
 
-  const { type, orm, file, src } = await inquirer.prompt(questions);
+  // const { type, orm, file, src } = await inquirer.prompt(questions);
+  const path = join(shelljs.pwd().stdout, "gen.json");
+
+  try {
+    const data: databaseI = jsonFile.readFileSync(path);
+    shelljs.mkdir("-p", "src/entities");
+
+    data.database.map((table: tableDataI) => {
+      shelljs.touch(
+        join(shelljs.pwd().stdout, "src", "entities", `${table.name}.entity.ts`)
+      );
+    });
+  } catch (error) {
+    console.error(error);
+  }
 };
+
+interface databaseI {
+  version: string;
+  database: tableDataI[];
+}
+
+interface tableDataI {
+  name: string;
+  columns: string[];
+  primary: string;
+  relations: string[];
+}
 
 init();
